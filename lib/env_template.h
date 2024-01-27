0c3f609ad9313bb1d156e7d6d5222d8e9d9a9722
gos <6231546+aresta@users.noreply.github.com>
2024-01-27 13:18:33 +0100

    wip

---
+++ lib/env_template.h
@@ -0,0 +1,32 @@
+#ifndef env_
+#define env_
+
+// #define WIFI_SSID "YOUR WIFI"
+// #define WIFI_PASSWORD "YOUR PWD"
+
+// static IP
+// #define IP 192,168,1,10
+// #define DNS 192,168,1,1
+// #define GATEWAY 192,168,1,1
+// #define SUBNET 255,255,255,0
+
+// #define THINGNAME ""
+// const char AWS_MQTT_HOST[] = "";
+
+// #define AWS_IOT_PUBLISH_TOPIC ""
+// #define AWS_IOT_PUBLISH_LOG_INFO ""
+// #define AWS_IOT_PUBLISH_LOG_ERROR ""
+
+// Amazon Root CA 1
+// static const char AWS_CERT_CA[] PROGMEM = R"EOF(
+// )EOF";
+
+// // Device Certificate                                              
+// static const char AWS_CERT_CRT[] PROGMEM = R"EOF(
+// )EOF";
+	
+// // Device Private Key                                              
+// static const char AWS_CERT_PRIVATE[] PROGMEM = R"EOF(
+// )EOF";
+
+#endif
